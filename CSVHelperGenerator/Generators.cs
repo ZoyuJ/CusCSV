0909fe973af86fd89c0c7e4fd8f7655562798529
ZoyuJ <yuhanjie@live.com>
2021-04-06 09:15:55 +0800

    _stage

--- CSVHelperGenerator/Generators.cs
+++ CSVHelperGenerator/Generators.cs
@@ -10,7 +10,7 @@
   using System.Diagnostics;
   using System.Linq;
   using System.Text;
-
+  
   //  [Generator]
   //  public class Gen : ISourceGenerator {
   //    public void Initialize(GeneratorInitializationContext context) {
